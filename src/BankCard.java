efb5aac46716da4e30282c9d7f509dbdaa99de75
TONGMOON\彤彤 <986098090@qq.com>
2018-03-31 22:58:13 +0800

    fdfa

---
+++ src/BankCard.java
@@ -0,0 +1,31 @@
+
+public class BankCard {
+	
+	public int id;
+	public String name;
+	public String email;
+	public String age;
+	
+	private double saraly;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSaraly() {
+		return saraly;
+	}
+	public void setSaraly(double saraly) {
+		this.saraly = saraly;
+	}
+	
+	
+
+}
---
+++ src/Home_01/home.java
@@ -0,0 +1,10 @@
+package Home_01;
+/**
+ * @date time  2018-3-18
+ * @author ͮͮ
+ *
+ */
+public class home {
+	
+
+}
